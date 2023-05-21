50ff114f47560cbf32c4d7c6b0b8d748fe0a2a06
Tatiana <tatyanakoroleva00@mail.ru>
2023-05-21 15:10:56 +0300

    'done'

--- Code.mu Trepachev Dmitry/Forms/index.php
+++ Code.mu Trepachev Dmitry/Forms/index.php
@@ -1,1 +1,27 @@
 <?php
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Practice</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+<!--<form class="form" action="result.php">-->
+<!--    <div><label for="name">Name: </label><input name ="name" type="text"></div>-->
+<!--    <div><label for="name">Age: <input name="age" type="text"></label></div>-->
+<!--    <div><label for="salary">Salary: <input name="salary" type="number"></label></div>-->
+<!--    <input type="submit">-->
+<!--</form>-->
+
+<!--Сделайте форму с тремя инпутами. Пусть в эти инпуты вводятся числа. После отправки формы выведите на экран сумму этих чисел.-->
+<form class="form" action="result.php" method="post">
+    <div><input name ="num1"></div>
+    <div><input name="num2"></div>
+    <div><input name="num3"></div>
+    <input type="submit">
+</form>
+
+</body>
+</html>
