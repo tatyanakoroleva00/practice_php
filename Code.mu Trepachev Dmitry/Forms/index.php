91030390d231f57f8f95b48939940b355706378f
Tatiana <tatyanakoroleva00@mail.ru>
2023-05-21 18:12:01 +0300

    exercice is done

--- Code.mu Trepachev Dmitry/Forms/index.php
+++ Code.mu Trepachev Dmitry/Forms/index.php
@@ -16,12 +16,33 @@
 <!--</form>-->
 
 <!--Сделайте форму с тремя инпутами. Пусть в эти инпуты вводятся числа. После отправки формы выведите на экран сумму этих чисел.-->
-<form class="form" action="result.php" method="post">
-    <div><input name ="num1"></div>
-    <div><input name="num2"></div>
-    <div><input name="num3"></div>
-    <input type="submit">
-</form>
+<!--<form class="form" action="result.php" method="post">-->
+<!--    <div><input name ="num1"></div>-->
+<!--    <div><input name="num2"></div>-->
+<!--    <div><input name="num3"></div>-->
+<!--    <input type="submit">-->
+<!--</form>-->
+
+<!--С помощью формы спросите у пользователя его имя и возраст. После отправки формы выведите эти данные на экран.-->
+
+<!--<form class="form" action="result.php" method="POST">-->
+<!--    <label for="name">What is your name?</label><input name="name">-->
+<!--    <label for="age">What is you age?</label> <input name="age">-->
+<!--    <input type="submit">-->
+<!--</form>-->
+
+<!--<form action="result.php" method="POST">-->
+<!--    <input type="password" name="pass">-->
+<!--    <input type="submit">-->
+<!--</form>-->
+<!---->
+
+
+
+
+
+
+
 
 </body>
 </html>
--- Code.mu Trepachev Dmitry/Forms/result.php
+++ Code.mu Trepachev Dmitry/Forms/result.php
@@ -1,2 +1,10 @@
 <?php
-echo "Потрясающий результат ввода данных в форму: " . ($_POST['num1'] + $_POST['num2'] + $_POST['num3']);
+//echo "Потрясающий результат ввода данных в форму: " . ($_POST['num1'] + $_POST['num2'] + $_POST['num3']);
+
+//echo "Your name is: $_POST[name]. Your age is: $_POST[age]";
+
+//$pass = '12345';
+//
+//if ($pass === $_POST['pass']) {
+//    echo 'password is correct';
+//} else echo 'password is not correct';
